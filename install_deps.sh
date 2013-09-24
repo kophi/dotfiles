9586827a17a79edb4c354d689220e4e037e56264
Philipp Kopf <philipp.kopf@gmail.com>
2013-09-24 22:22:57 +0200

    Customizing :)

---
+++ install_deps.sh
@@ -0,0 +1,11 @@
+# https://github.com/jamiew/git-friendly
+# the `push` command which copies the github compare URL to my clipboard is heaven
+sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
+
+# homebrew!
+# you need the code CLI tools YOU FOOL.
+ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
+
+# for the c alias (syntax highlighted cat)
+sudo easy_install Pygments
+
